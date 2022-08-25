ee581aed3f3b4ba0ebc7ba2a0db124f1fb359d4f
Louise <louiserafaellalu@gmail.com>
2022-08-25 16:45:34 +0800

    +1: disable landscape mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:nft_ticketing/nft_ticketing_app.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+
   runApp(NFTTicketingApp());
 }
