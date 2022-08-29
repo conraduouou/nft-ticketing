939d0d1e6d7da36be9956838d7c0ff6644300c02
Louise <louiserafaellalu@gmail.com>
2022-08-29 22:32:32 +0800

    +1: create draggable scrollable bottom sheet in create post page; show different set of icons when keyboard is up

--- lib/screens/dashboard/create_post_page/create_post_page.dart
+++ lib/screens/dashboard/create_post_page/create_post_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:nft_ticketing/components/nft_back_button.dart';
 import 'package:nft_ticketing/components/nft_button.dart';
 import 'package:nft_ticketing/constants.dart';
@@ -13,50 +15,180 @@ class CreatePostPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kDarkBlue,
-      bottomSheet: Container(
-        color: kDarkBlue,
-        child: Container(
-          height: 200,
-          decoration: const BoxDecoration(
-            color: kSlightlyDarkBlue,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(20),
-              topRight: Radius.circular(20),
-            ),
-          ),
-        ),
-      ),
-      body: Column(
+      body: Stack(
+        fit: StackFit.expand,
         children: [
-          const _NFTCreatePostPagePseudoBar(),
-          Expanded(
-            child: CustomScrollView(
-              slivers: [
-                const SliverPadding(
-                  padding: EdgeInsets.only(left: 20, top: 30, bottom: 40),
-                  sliver: SliverToBoxAdapter(
-                    child: _NFTCreatePostPageConfig(),
-                  ),
-                ),
-                SliverToBoxAdapter(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    child: TextField(
-                      maxLines: null,
-                      style: kRegularStyle.copyWith(
-                        color: Colors.white,
-                      ),
-                      decoration: const InputDecoration(
-                        border: InputBorder.none,
+          Column(
+            children: [
+              const _NFTCreatePostPagePseudoBar(),
+              Expanded(
+                child: CustomScrollView(
+                  slivers: [
+                    const SliverPadding(
+                      padding: EdgeInsets.only(left: 20, top: 30, bottom: 40),
+                      sliver: SliverToBoxAdapter(
+                        child: _NFTCreatePostPageConfig(),
                       ),
                     ),
-                  ),
+                    SliverToBoxAdapter(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        child: TextField(
+                          maxLines: null,
+                          style: kRegularStyle.copyWith(
+                            color: Colors.white,
+                          ),
+                          decoration: const InputDecoration(
+                            border: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+              KeyboardVisibilityBuilder(
+                builder: (_, isVisible) {
+                  if (!isVisible) {
+                    return const _NFTCreatePostPageBottomSheet();
+                  }
+
+                  return const _NFTCreatePostPageActions();
+                },
+              ),
+            ],
           ),
         ],
       ),
+    );
+  }
+}
+
+class _NFTCreatePostPageActions extends StatelessWidget {
+  const _NFTCreatePostPageActions({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20, bottom: 20),
+      child: Row(
+        children: [
+          SvgPicture.asset('assets/icons/ic-take-a-picture.svg'),
+          const SizedBox(width: 20),
+          SvgPicture.asset('assets/icons/ic-upload-image.svg'),
+          const SizedBox(width: 20),
+          SvgPicture.asset('assets/icons/ic-take-a-video.svg'),
+        ],
+      ),
+    );
+  }
+}
+
+class _NFTCreatePostPageBottomSheet extends StatelessWidget {
+  const _NFTCreatePostPageBottomSheet({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: DraggableScrollableSheet(
+        initialChildSize: 0.25,
+        maxChildSize: 1,
+        expand: true,
+        snap: true,
+        snapSizes: const [0.25, 1],
+        builder: (context, scrollController) {
+          return Container(
+            color: kDarkBlue,
+            child: Container(
+              height: 200,
+              decoration: const BoxDecoration(
+                color: kSlightlyDarkBlue,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
+              ),
+              child: Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  Align(
+                    alignment: Alignment.topCenter,
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 10),
+                      child: Container(
+                        height: 5,
+                        width: 50,
+                        decoration: BoxDecoration(
+                          color: kSecondaryColor,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                  ),
+                  // the actual list of items
+                  ListView(
+                    padding: const EdgeInsets.only(top: 35, left: 20),
+                    physics: const NeverScrollableScrollPhysics(),
+                    children: const [
+                      _NFTCreatePostPageBottomSheetAction(
+                        assetPath: 'assets/icons/ic-take-a-picture.svg',
+                        actionText: 'Take a picture',
+                      ),
+                      SizedBox(height: 20),
+                      _NFTCreatePostPageBottomSheetAction(
+                        assetPath: 'assets/icons/ic-upload-image.svg',
+                        actionText: 'Add a photo',
+                      ),
+                      SizedBox(height: 20),
+                      _NFTCreatePostPageBottomSheetAction(
+                        assetPath: 'assets/icons/ic-take-a-video.svg',
+                        actionText: 'Take a video',
+                      ),
+                    ],
+                  ),
+                  // to enable the bottom sheet's draggable behavior
+                  ListView(controller: scrollController),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class _NFTCreatePostPageBottomSheetAction extends StatelessWidget {
+  const _NFTCreatePostPageBottomSheetAction({
+    Key? key,
+    required this.assetPath,
+    required this.actionText,
+  }) : super(key: key);
+
+  final String assetPath;
+  final String actionText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SizedBox(
+          width: 18,
+          child: SvgPicture.asset(assetPath),
+        ),
+        const SizedBox(width: 16),
+        Text(
+          actionText,
+          style: kRegularStyle.copyWith(
+            color: Colors.white,
+          ),
+        ),
+      ],
     );
   }
 }
