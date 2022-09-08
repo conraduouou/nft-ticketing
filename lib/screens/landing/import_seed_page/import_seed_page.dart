0a6b70f352c98319e90d889ffaec95587ec2c6a4
Louise <louiserafaellalu@gmail.com>
2022-09-08 20:05:06 +0800

    +1: finish password strength

--- lib/screens/landing/import_seed_page/import_seed_page.dart
+++ lib/screens/landing/import_seed_page/import_seed_page.dart
@@ -3,7 +3,9 @@ import 'package:nft_ticketing/components/nft_appbar.dart';
 import 'package:nft_ticketing/components/nft_button.dart';
 import 'package:nft_ticketing/components/nft_field.dart';
 import 'package:nft_ticketing/constants.dart';
+import 'package:nft_ticketing/providers/new_password_provider.dart';
 import 'package:nft_ticketing/screens/landing/connect_wallet_page/connect_wallet_page.dart';
+import 'package:provider/provider.dart';
 
 class ImportSeedPage extends StatelessWidget {
   const ImportSeedPage({super.key});
@@ -12,25 +14,44 @@ class ImportSeedPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: kDarkBlue,
-      appBar: const NFTAppBar(title: 'Import from seed phrase'),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: const [
-            SizedBox(height: 25),
-            _NFTImportSeedPageCustomField(),
-            SizedBox(height: 20),
-            NFTField(isObscurable: true, hintText: 'New password'),
-            SizedBox(height: 20),
-            NFTField(isObscurable: true, hintText: 'Confirm password'),
-            SizedBox(height: 50),
-            NFTButton(text: 'Import wallet')
-          ],
-        ),
-      ),
+    return ChangeNotifierProvider(
+      create: (context) => NewPasswordProvider(),
+      builder: (context, child) {
+        final provider = context.read<NewPasswordProvider>();
+
+        return Scaffold(
+          backgroundColor: kDarkBlue,
+          appBar: const NFTAppBar(title: 'Import from seed phrase'),
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const SizedBox(height: 25),
+                  const _NFTImportSeedPageCustomField(),
+                  const SizedBox(height: 20),
+                  NFTField(
+                    hintText: 'New password',
+                    isObscurable: true,
+                    onChanged: provider.onNewChange,
+                  ),
+                  const SizedBox(height: 10),
+                  const _NFTImportSeedPagePasswordStrength(),
+                  const SizedBox(height: 20),
+                  NFTField(
+                    hintText: 'Confirm password',
+                    isObscurable: true,
+                    onChanged: provider.onConfirmChange,
+                  ),
+                  const SizedBox(height: 50),
+                  const NFTButton(text: 'Import wallet')
+                ],
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
@@ -96,6 +117,76 @@ class _NFTImportSeedPageCustomFieldState
           const SizedBox(width: 15),
         ],
       ),
+    );
+  }
+}
+
+class _NFTImportSeedPagePasswordStrength extends StatelessWidget {
+  const _NFTImportSeedPagePasswordStrength({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<NewPasswordProvider>(
+      builder: (_, provider, __) {
+        final width = MediaQuery.of(context).size.width;
+        final requirements = provider.requirements;
+        final satisfiedReqs = requirements.where((e) => e.isSatisfied);
+
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              height: 5,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25),
+                color: kSlightlyDarkBlue,
+              ),
+              alignment: Alignment.centerLeft,
+              child: Container(
+                width: satisfiedReqs.length == 1
+                    ? width / 3
+                    : satisfiedReqs.length == 2
+                        ? width * 2 / 3
+                        : satisfiedReqs.length == 3
+                            ? width
+                            : 0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(25),
+                  color: Colors.green,
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Password Strength',
+                  style: kRegularStyle.copyWith(
+                    color: Colors.white.withAlpha(120),
+                    fontSize: kRegularSize - 6,
+                  ),
+                ),
+                Text(
+                  satisfiedReqs.length == 3
+                      ? 'Strong'
+                      : satisfiedReqs.length == 2
+                          ? 'Moderately strong'
+                          : satisfiedReqs.length == 1
+                              ? 'Moderately weak'
+                              : 'Weak',
+                  style: kRegularStyle.copyWith(
+                    color: Colors.green,
+                    fontSize: kRegularSize - 6,
+                  ),
+                )
+              ],
+            )
+          ],
+        );
+      },
     );
   }
 }
