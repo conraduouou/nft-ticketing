485ecd80b1a487eeebaf0b16dff1ad31dd36e911
Louise <louiserafaellalu@gmail.com>
2022-09-08 22:46:38 +0800

    +1: add placeholder QR code in qr code page

--- lib/screens/dashboard/ticket_view_page/qr_code_page.dart
+++ lib/screens/dashboard/ticket_view_page/qr_code_page.dart
@@ -3,6 +3,10 @@ import 'package:nft_ticketing/components/nft_appbar.dart';
 import 'package:nft_ticketing/constants.dart';
 import 'package:nft_ticketing/screens/dashboard/ticket_view_page/ticket_view_page.dart';
 
+// NOTE: PLACEHOLDER QR CODE IS IN PLACE IN THE CODE, unexpected
+// things may happen in the case the QR code is read. Proceed with
+// caution.
+
 class QRCodePage extends StatelessWidget {
   const QRCodePage({
     super.key,
@@ -15,9 +19,32 @@ class QRCodePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
       appBar: NFTAppBar(title: eventTitle),
       backgroundColor: kDarkBlue,
+      body: Column(
+        children: [
+          const SizedBox(height: 20),
+          Center(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                maxWidth: size.width - 40,
+                maxHeight: size.width - 40,
+              ),
+              child: Container(
+                color: Colors.white,
+                padding: const EdgeInsets.all(35),
+                child: Image.asset(
+                  'assets/sample_qr.png',
+                  fit: BoxFit.scaleDown,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
